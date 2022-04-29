db222a45e0a14487d1ebd3cad5c543c15e6ef9df
Hazey87 <jfhazey87@outlook.com>
2022-04-28 22:35:15 -0400

    commit

--- Week-1/math/app.js
+++ Week-1/math/app.js
@@ -1,1 +1,16 @@
 //! JavaScript Page (app.js)
+
+//localhost:3000/
+
+// http: var express = require("express");
+// var app = express();
+// const port = 3000;
+var math = require("./math");
+
+console.log(`math addition =  ${math.addition(3, 4)}`);
+
+console.log(`math multiplication = ${math.multiplication(3, 4)}`);
+
+console.log(`math division = ${math.division(3, 4)}`);
+
+console.log(`math subtraction = ${math.subtraction(3, 4)}`);
--- Week-1/math/math.js
+++ Week-1/math/math.js
@@ -3,3 +3,22 @@
 const addition = (a, b) => {
     return a + b;
 };
+
+const multiplication = (a, b) => {
+    return a * b;
+};
+
+const division = (a, b) => {
+    return a / b;
+};
+
+const subtraction = (a, b) => {
+    return a - b;
+};
+
+module.exports = {
+    addition,
+    multiplication,
+    division,
+    subtraction,
+};
