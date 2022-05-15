d18540497dc86817a4677214c8082244924f0cdd
Hazey87 <jfhazey87@outlook.com>
2022-05-14 23:23:39 -0400

    commit

---
+++ Week-4/express-router-2/recycleBin.js
@@ -0,0 +1,38 @@
+const RecycledItems = [
+    {
+        Id: 1,
+        Name: "Bottle",
+        Description: "Plastic bottle",
+        Recyclable: true,
+        Quantity: "685",
+        Price_Per_Unit: 85,
+    },
+    {
+        Id: 2,
+        Name: "Bottle",
+        Description: "Glass bottle",
+        Recyclable: true,
+        Quantity: "435",
+        Price_Per_Unit: 55,
+    },
+    {
+        Id: 3,
+        Name: "Can",
+        Description: "Soda Can",
+        Recyclable: true,
+        Quantity: "65",
+        Price_Per_Unit: 15,
+    },
+    {
+        Id: 4,
+        Name: "Card Board",
+        Description: "Moving Box",
+        Recyclable: true,
+        Quantity: "1115",
+        Price_Per_Unit: 5,
+    },
+];
+
+module.exports = {
+    RecycledItems,
+};
