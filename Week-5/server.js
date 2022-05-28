907af610cb8049467f233b8238b8bc332823bc66
Hazey87 <jfhazey87@outlook.com>
2022-05-28 19:38:57 -0400

    commit

--- Week-5/server.js
+++ Week-5/server.js
@@ -3,6 +3,7 @@ const bell = express();
 
 const bountyRouter = require("./routes/bountyRouter");
 
+//! Listener PORT 3000
 bell.listen(3000, () => {
     console.log(
         "listening on PORT 3000" + " " + "Begin your Project GOOD LUCK! "
@@ -11,4 +12,5 @@ bell.listen(3000, () => {
 
 bell.use(express.json());
 
+//! Routes
 bell.use("/recycledItems ", bountyRouter);
