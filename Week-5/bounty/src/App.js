f62a7057bcacf6c9cff735bba271e2c00aecc654
Hazey87 <jfhazey87@outlook.com>
2022-05-29 22:00:58 -0400

    commit

--- Week-5/bounty/src/App.js
+++ Week-5/bounty/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const PORT = 3000;
+
 function App() {
     return (
         <div className='App'>
             <header className='App-header'>
                 <img src={logo} className='App-logo' alt='logo' />
-                <h1>React Running on "PORT:3000"</h1>
+                <h1>React Running on "PORT:{PORT}"</h1>
                 <h3>Start your Project Jordan, Good Luck!</h3>
             </header>
         </div>
