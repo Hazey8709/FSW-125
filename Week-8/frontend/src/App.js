e5cee785b75ca68128d026ceb19ae3bcf4a3cf67
Hazey87 <jfhazey87@outlook.com>
2022-06-03 16:00:53 -0400

    commit

--- Week-8/frontend/src/App.js
+++ Week-8/frontend/src/App.js
@@ -1,17 +1,75 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Item from "./components/Item";
+import ItemFormHandler from "./components/ItemFormHandler";
 import "./App.css";
 
-const PORT = 3000;
-
 function App() {
+    const [items, setItems] = useState([]);
+
+    const getItems = () => {
+        axios
+            .get("/teams")
+            .then((res) => setItems(res.data))
+            .catch((err) => console.log(err));
+    };
+
+    const addItem = (newItem) => {
+        axios
+            .post("/teams", newItem)
+            .then((res) => {
+                setItems((prevItem) => [...prevItem, res.data]); //Causes-ReRender
+            })
+            .catch((err) => console.log(err));
+    };
+
+    const editItem = (updates, itemId) => {
+        console.log("wtf: " + itemId);
+        axios
+            .put(`/teams/${itemId}`, updates)
+            .then((res) => {
+                setItems((prevItem) =>
+                    prevItem.map((item) =>
+                        item._id !== itemId ? item : res.data
+                    )
+                );
+            })
+            .catch((err) => console.log(err));
+    };
+    const deleteItem = (itemId) => {
+        axios
+            .delete(`/teams/${itemId}`)
+            .then((res) => {
+                setItems((prevItem) =>
+                    prevItem.filter((item) => item._id !== itemId)
+                );
+            })
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
+    const itemList = items.map((item) => (
+        <Item
+            {...item}
+            deleteItem={deleteItem}
+            editItem={editItem}
+            key={item.name}
+        />
+    ));
+
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <h1>React Running on "PORT:{PORT}"</h1>
-                <h3>Start your Project Jordan, Good Luck!</h3>
-            </header>
-            <main></main>
+        <div className='itemContainer'>
+            <h1>Team React Application</h1>
+            <navbar>
+                <hr />
+                <ItemFormHandler btnText='Add a Team' submit={addItem} />
+                <hr />
+            </navbar>
+            {itemList}
+            <hr />
         </div>
     );
 }
